6ff5e796945d6c109854be7ec608d6773b04de4d
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-07-08 10:39:26 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,15 +1,12 @@
 class RepositoriesController < ApplicationController
   def index
-    # response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    # @repos_array = JSON.parse(response.body)
     github = GithubService.new
     @repos_array = github.get_repos(session[:token])
   end
 
   def create
-    github = GithubService.new(params[:name], session[:token])
-
-    # response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    github = GithubService.new()
+    github.create_repo(params, token)
     redirect_to '/'
   end
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -11,33 +11,26 @@ class GithubService
 #creates a new sesssion - responsible for session token
   def authenticate!(client_id, client_secret, code)
     response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret, code: code}, {'Accept' => 'application/json'}
-
-    access_hash = JSON.parse(response.body)
-    @access_token = access_hash["access_token"]
-    # access_hash["access_token"]
-    @access_token
+    JSON.parse(response.body)
   end
 
 #responsible for getting sesssion[;username]
   def get_username(token)
 
-    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => token, 'Accept' => 'application/json'}
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization'=>"token #{token}", 'Accept' => 'application/json'}
     user_json = JSON.parse(user_response.body)
-    # session[:username] = user_json["login"]
     user_json["login"]
   end
 
 #responsible for getting @repos_array
   def get_repos(token)
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => token, 'Accept' => 'application/json'}
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization'=>"token #{token}", 'Accept' => 'application/json'}
     body = JSON.parse(response.body)
-    # binding.pry
   end
 
-
 #creates a new repo on github
-  def create_repo(name, token)
-    response = Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => token, 'Accept' => 'application/json'}
+  def create_repo(params, token)
+    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization'=>"token #{token}", 'Accept' => 'application/json'}
   end
 
 
